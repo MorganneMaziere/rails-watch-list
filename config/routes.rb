8535b7f41b19a68f9c68ee03743c6acd81d0f64e
Morganne Maziere <morganne.maziere@kedgebs.com>
2023-06-17 11:12:20 +0200

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :lists, only: %i[index new show create destroy] do
     resources :bookmarks, only: %i[new create]
+    resources :reviews, only: [:create]
   end
   resources :bookmarks, only: [:destroy]
 end
